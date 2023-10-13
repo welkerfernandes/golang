f867c71434b858981e08e1befbf8c74379caa2b3
welkerfernandes <welkerfc@icloud.com>
2023-10-13 09:24:54 -0300

    println variavel vazia

--- inicio.go
+++ inicio.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//aqui começa o programa
+	//pacote . identificador
+	// estou usando o _, no inicio para eliminar uma variável vazia pois no golang é proibida variavel vazia
+	_, erros := fmt.Println("Hello World", "Oi Galera", 100)
+	fmt.Println(erros)
+	//aqui acaba seu programa
+
 }
