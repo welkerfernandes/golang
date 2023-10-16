fb925eec6fc74d103c286b37d1eb3ae3ed5db2ff
welkerfernandes <welkerfc@icloud.com>
2023-10-16 10:50:56 -0300

    exercicio binario decimal e hexadecimal

---
+++ exercicio11.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := 150
+
+	fmt.Println("binário\t\tdecimal\t\thexa-decimal")
+
+	//binário decimal e hexadecimal
+	fmt.Printf("%b\t\t", a)
+	fmt.Printf("%d\t\t", a)
+	fmt.Printf("%X\t\t", a)
+
+}
