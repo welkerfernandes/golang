52a6e4ed844451c2e3beb6aec36a55b343f8ee2b
welkerfernandes <welkerfc@icloud.com>
2023-10-16 23:46:14 -0300

    string 2

--- string2.go
+++ string2.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-	s := "Welker é o Brabo"
+	s := "welker"
 
 	for _, v := range s {
 		fmt.Printf("%b - %v - %T - %#U - %#X\n", v, v, v, v)
@@ -17,4 +17,5 @@ func main() {
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("%b - %v - %T - %#U - %#X\n", s[i], s[i], s[i], s[i])
 	}
+
 }
