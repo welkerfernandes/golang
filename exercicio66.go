85b7d7ab1c14ab5c71894508f1261f1d1b8e070d
welkerfernandes <welkerfc@icloud.com>
2023-10-17 00:07:20 -0300

    iota

---
+++ exercicio66.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	_ = iota
+	ano1
+	ano2
+	ano3
+	ano4
+)
+
+func main() {
+
+	fmt.Println("Ano: ", ano1, "\nAno: ", ano2, "\nAno: ", ano3, "\nAno: ", ano4)
+
+}
