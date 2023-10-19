f5bf551a9c97714a4d62b6dc9057820447913671
welkerfernandes <welkerfc@icloud.com>
2023-10-19 09:29:31 -0300

    switch nada demais

---
+++ switch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := 10
+
+	switch true {
+
+	case x > 10:
+		fmt.Println("maior que 10")
+	case x == 50:
+		fmt.Println("igual a 50")
+	case x < 20:
+		fmt.Println("menor que 20")
+
+	}
+
+}
