c1ea456ec5aa8ece6c3f6e58a46f7e5fa431e919
welkerfernandes <welkerfc@icloud.com>
2023-10-19 09:03:03 -0300

    nao existe while em go

--- while.go
+++ while.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// em go não existe while mas pode ser feito o for dessa forma descrita abaixo
+// em go não existe while mas pode ser feito o for dessas forma descritas abaixo
 func main() {
 
 	x := 0
@@ -12,6 +12,20 @@ func main() {
 	for x <= 10 {
 		fmt.Println("Você é vencedor")
 		x++
+	}
+
+	//aqui utilizo for sem condição
+	a := 0
+
+	for {
+
+		if a < 10 {
+			a++
+			fmt.Println(a)
+		} else {
+			break
+		}
+
 	}
 
 }
