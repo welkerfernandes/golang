7cf2935b280167c7cbacd7f2a43989bbaebd5f2c
welkerfernandes <welkerfc@icloud.com>
2023-11-02 07:59:11 -0300

    ponteiros

--- ponteiros.go
+++ ponteiros.go
@@ -15,4 +15,17 @@ func main() {
 	fmt.Printf("%T,%T\n", x, y)
 	fmt.Println(x, y)
 
+	recebevalor(x)
+	recebe_ponteiro(&x)
+
+}
+
+func recebevalor(x int) {
+	x++
+	fmt.Println(x)
+}
+
+func recebe_ponteiro(x *int) {
+	*x++
+	fmt.Println(x)
 }
